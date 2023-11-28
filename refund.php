0a5bc928fdc5e1b7e9963b015e76bd9c8352d595
eslam-1-adel <eslamadel3052002@gmail.com>
2023-11-28 23:04:39 +0200

    title changed

--- refund.php
+++ refund.php
@@ -18,7 +18,7 @@ if(isset($_POST['ticket_id'])){
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>cinema centre</title>
+  <title>My Ticket</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
